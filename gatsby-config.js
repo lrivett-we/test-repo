a550b3e1c978573fc08127b8056b5b07c18246ce
Luke Rivett <lrivett-we>
2018-09-13 13:58:31 -0400

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+      {
+        resolve: 'gatsby-source-storyblok',
+        options: {
+          accessToken: 'cD5ZR4CQ4mgsfdrc2dOb2Att',
+          homeSlug: 'home',
+          version: 'draft'
+        }
+      }
+    ]
+  }
